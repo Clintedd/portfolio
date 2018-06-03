437237aab12e57d2b7a586df18217518bba6250a
Clint Lee <l.clint91@gmail.com>
2018-06-03 18:33:11 -0400

    added typeIt library

---
+++ dev/scripts/main.js
@@ -0,0 +1,20 @@
+//TypeIt javascript starts//
+
+const headerHello = new TypeIt('#typeHello', {
+  strings: ["Hello! I'm"],
+  speed: 150,
+  cursor: false
+});
+
+const headerName = new TypeIt('#typeName', {
+  strings: ['Clint Lee', 'Web Developer', 'Clint Lee'],
+  speed: 150,
+  breakLines: false,
+  deleteSpeed: 100,
+  cursor: false
+});
+
+headerHello();
+headerName();
+
+//TypeIt javascript ends//
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+
+//TypeIt javascript starts//
+
+var headerHello = new TypeIt('#typeHello', {
+  strings: ["Hello! I'm"],
+  speed: 150,
+  cursor: false
+});
+
+var headerName = new TypeIt('#typeName', {
+  strings: ['Clint Lee', 'Web Developer', 'Clint Lee'],
+  speed: 150,
+  breakLines: false,
+  deleteSpeed: 100,
+  cursor: false
+});
+
+headerHello();
+headerName();
+
+//TypeIt javascript ends//

bdc26d03e3af2b069d682ae4581d996b4287ad9e
Clint Lee <l.clint91@gmail.com>
2018-06-05 13:01:32 -0400

    responsive

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -28,16 +28,49 @@ app.headerNameKo = new TypeIt('#typeNameKo', {
 
 //TypeIt javascript ends//
 
-app.smooth = function () {
-  $('a').smoothScroll();
+app.menuToggle = function () {
+  $('.toggleMenu').on('click', function () {
+    $('header ul').toggleClass('open');
+  });
+};
+
+app.smoothScroll = function () {
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]').not('[href="#0"]').click(function (event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 };
 
 app.init = function () {
-  // app.headerHello();
-  // app.headerName();
-  // app.headerNameKo();
-  console.log('hi');
-  app.smooth();
+  app.smoothScroll();
+  app.menuToggle();
 };
 
 $(function () {
